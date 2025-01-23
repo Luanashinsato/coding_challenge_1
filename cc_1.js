d72b925618ba8054edd88b4c38cf87a86d5b9fb7
Luanashinsato <lshinsato@usf.edu>
2025-01-23 17:44:40 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,8 @@
+// Task 1: Employee Information 
+let employeeName = "Sophia Carter";
+console.log("Employee Name:", employeeName, "- Type:", typeof employeeName);
+const employeeID = 230;
+console.log("Employee ID:", employeeID, "- Type:", typeof employeeID);
+var isActive = true;
+console.log("Is Active:", isActive, "- Type:", typeof isActive);
+
