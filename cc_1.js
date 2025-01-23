d87c1781614530591daf7a18e2a5ec54b1716e2c
Luanashinsato <lshinsato@usf.edu>
2025-01-23 18:14:44 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -6,3 +6,11 @@ console.log("Employee ID:", employeeID, "- Type:", typeof employeeID);
 var isActive = true;
 console.log("Is Active:", isActive, "- Type:", typeof isActive);
 
+// Task 2: Product Details
+let productName = "Graphic Tshirt";
+console.log ("Product Name:", productName, "- Type:", typeof productName);
+const productPrice = 18;
+console.log("Product Price:", productPrice, "- Type:", typeof productPrice);
+var isAvailable = false;
+console.log("Is Available:", isAvailable, "- Type:", typeof isAvailable);
+
